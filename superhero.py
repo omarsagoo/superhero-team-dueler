f13cddf901a8c52a4adf27f312818b12ee0d6629
omarsagoo <om.sagoo@gmail.com>
2019-09-25 16:56:18 -0700

    add armor and hero class

--- superhero.py
+++ superhero.py
@@ -6,17 +6,55 @@ class Ability:
           name:String
           max_damage: Integer
         '''
-       # TODO: Instantiate the variables listed in the docstring with then
-       # values passed in
         self.name = name
         self.max_damage = attack_strength
 
     def attack(self):
       ''' Return a value between 0 and the value set by self.max_damage.'''
-      # TODO: Use random.randint(a, b) to select a random attack value.
       # Return an attack value between 0 and the full attack.
-      # Hint: The constructor initializes the maximum attack value.
       return random.randint(0,self.max_damage)
+
+class Armor:
+    def __init__(self, name, max_block):
+        '''Instantiate instance properties.
+            name: String
+            max_block: Integer
+        '''
+        self.name = name
+        self.max_block = max_block
+
+    def block(self):
+        ''' Return a random value between 0 and the initialized max_block strength. '''
+        return random.randint(0, self.max_block)
+
+class Hero:
+    def __init__(self, name, starting_health=100):
+        '''Instance properties:
+          abilities: List
+          armors: List
+          name: String
+          starting_health: Integer
+          current_health: Integer
+        '''
+        self.name = name
+        self.max_health = starting_health
+        self.current_health = self.max_health
+        self.armor = list()
+        self.abilities = list()
+
+    def add_ability(self, ability):
+        ''' Add ability to abilities list '''
+        self.abilities.append(ability)
+
+    def attack(self):
+        '''Calculate the total damage from all ability attacks.
+          return: total:Int
+        '''
+        # TODO: This method should run Ability.attack() on every ability
+        # in self.abilities and returns the total as an integer.
+        pass
+
+
 
 if __name__ == "__main__":
     # If you run this file from the terminal
@@ -24,3 +62,19 @@ if __name__ == "__main__":
     ability = Ability("Debugging Ability", 20)
     print(ability.name)
     print(ability.attack())
+
+    armor = Armor("Debugging Ability Armor", 20)
+    print(armor.name, armor.block())
+
+    my_hero = Hero("Grace Hopper", 200)
+    print(my_hero.name)
+    print(my_hero.current_health)
+
+    ability = Ability("Great Debugging", 50)
+    hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    print(hero.abilities)
+    
+
+
+
